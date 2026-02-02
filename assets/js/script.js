775c4abb8ad6d416d0ac0dad4e8b26ab2894557b
TaoGuo1998 <guotaomed@gmail.com>
2026-02-02 21:56:48 +0800

    feat: 添加了关于我的页面

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const mobileMenu = document.getElementById('mobile-menu');
+    const navLinks = document.querySelector('.nav-links');
+
+    if (mobileMenu) {
+        mobileMenu.addEventListener('click', () => {
+            navLinks.classList.toggle('active');
+        });
+    }
+});
